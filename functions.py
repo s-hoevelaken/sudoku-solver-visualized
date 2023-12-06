eef952b77a4eb36518366f307bfd2e00f7581dbf
Stephan Hoevelaken <msg01878@gmail.com>
2023-12-06 15:48:17 +0100

    completed functionality

--- functions.py
+++ functions.py
@@ -1,7 +1,5 @@
-def print_board(grid):
-    for row in grid:
-        print(row)
-
+import pygame
+import sys
 def find_empty_cell(grid):
     for i in range(9):
         for j in range(9):
@@ -30,21 +28,122 @@ def is_safe(grid, row, col, num):
         not used_in_subgrid(grid, row - row % 3, col - col % 3, num)
     )
 
+
 def solve_sudoku_using_backtracking(grid):
     selected_cell = find_empty_cell(grid)
 
     if not selected_cell:
-        return True
-
-    row, col = selected_cell
+        return grid
+    else:
+        row, col = selected_cell
 
     for num in range(1, 10):
         if is_safe(grid, row, col, num):
             grid[row][col] = num
 
             if solve_sudoku_using_backtracking(grid):
+                return grid
+
+            grid[row][col] = 0
+
+    return False
+
+
+def draw_grid(window, width, height, black):
+    cell_size = width // 9
+    for i in range(10):
+        thickness = 3 if i % 3 == 0 else 1
+        pygame.draw.line(window, black, (0, i * cell_size), (width, i * cell_size), thickness)
+        pygame.draw.line(window, black, (i * cell_size, 0), (i * cell_size, height), thickness)
+
+def draw_selected_cell(window, selected_cell, width, black):
+    if selected_cell is not None:
+        i, j = selected_cell
+        cell_size = width // 9
+        pygame.draw.rect(window, (0, 128, 255), (j * cell_size, i * cell_size, cell_size, cell_size), 3)
+
+def draw_numbers(window, grid, width, black):
+    font = pygame.font.Font(None, 36)
+    cell_size = width // 9
+
+    for i in range(9):
+        for j in range(9):
+            if grid[i][j] != 0:
+                text = font.render(str(grid[i][j]), True, black)
+                x = j * cell_size + (cell_size - text.get_width()) // 2
+                y = i * cell_size + (cell_size - text.get_height()) // 2
+                window.blit(text, (x, y))
+
+
+def solve_sudoku_using_backtracking_with_animation(grid, window, black, white, width, height):
+    selected_cell = find_empty_cell(grid)
+
+    if not selected_cell:
+        return True
+    else:
+        row, col = selected_cell
+
+    clock = pygame.time.Clock()
+    animation_speed = 10
+
+    for num in range(1, 10):
+        if is_safe(grid, row, col, num):
+            grid[row][col] = num
+
+            window.fill(white)
+            draw_grid(window, width, height, black)
+            draw_numbers(window, grid, width, black)
+            draw_selected_cell(window, (row, col), width, black)
+            pygame.display.flip()
+
+            clock.tick(animation_speed)
+
+            pygame.event.pump()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            if solve_sudoku_using_backtracking_with_animation(grid, window, black, white, width, height):
                 return True
 
             grid[row][col] = 0
 
     return False
+
+
+
+def show_mistake_prompt(window, black, white, width, height):
+    font = pygame.font.Font(None, 36)
+    text = font.render("Your board has mistakes. Choose an action:", True, black)
+    rect = text.get_rect(center=(width // 2, height // 2 - 50))
+    window.blit(text, rect)
+
+    options = ["Reset the board completely", "Go back to the last correct board and keep trying", "Solve last correct board"]
+    button_height = 50
+    button_width = 603
+    button_start_y = height // 2 + 20
+
+    for i, option in enumerate(options):
+        button_rect = pygame.Rect((width - button_width) // 2, button_start_y + i * button_height, button_width, button_height)
+        pygame.draw.rect(window, white, button_rect)
+        pygame.draw.rect(window, black, button_rect, 2)
+
+        text = font.render(option, True, black)
+        text_rect = text.get_rect(center=button_rect.center)
+        window.blit(text, text_rect)
+
+    pygame.display.flip()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                for i, option in enumerate(options):
+                    button_rect = pygame.Rect((width - button_width) // 2, button_start_y + i * button_height, button_width, button_height)
+                    if button_rect.collidepoint(x, y):
+                        return i + 1
--- main.py
+++ main.py
@@ -1,16 +1,110 @@
-from functions import print_board, solve_sudoku_using_backtracking
+from functions import draw_grid, draw_selected_cell, draw_numbers, solve_sudoku_using_backtracking, is_safe, find_empty_cell, solve_sudoku_using_backtracking_with_animation, show_mistake_prompt
+import pygame
+import sys
+import copy
+import time
 
-grid = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
 
-solve_sudoku_using_backtracking(grid)
-print_board(grid)
+pygame.init()
+
+width, height = 603, 603
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Sudoku!!")
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+initial_grid = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+
+
+grid_to_solve = copy.deepcopy(initial_grid)
+
+solved_successfully = solve_sudoku_using_backtracking(grid_to_solve)
+
+if not solved_successfully:
+    print("The provided Sudoku puzzle has no solution.")
+    pygame.quit()
+    sys.exit()
+
+
+player_grid = [row[:] for row in initial_grid]
+
+selected_cell = 0,0
+mistake_count = 0
+solving = False
+last_correct_grid = []
+has_mistakes = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            cell_size = width // 9
+            mouseX, mouseY = pygame.mouse.get_pos()
+            selected_cell = (mouseY // cell_size, mouseX // cell_size)
+        elif event.type == pygame.KEYDOWN:
+            i, j = selected_cell
+            if event.key == pygame.K_SPACE:
+                if has_mistakes:
+                    choice = show_mistake_prompt(window, black, white, width, height)
+                    if choice == 1:
+                        player_grid = [row[:] for row in initial_grid]
+                        mistake_count = 0
+                        has_mistakes = False
+                    elif choice == 2:
+                        player_grid = copy.deepcopy(last_correct_grid)
+                        mistake_count = 0
+                        has_mistakes = False
+                    elif choice == 3:
+                        player_grid = copy.deepcopy(last_correct_grid)
+                        solving = True
+                else:
+                    solving = True
+            elif event.key == pygame.K_BACKSPACE and player_grid[i][j] != 0 and initial_grid[i][j] == 0:
+                player_grid[i][j] = 0
+            elif selected_cell is not None and pygame.K_1 <= event.key <= pygame.K_9:
+                if initial_grid[i][j] == 0 and is_safe(player_grid, i, j, int(pygame.key.name(event.key))):
+                    if int(pygame.key.name(event.key)) != solved_successfully[i][j]:
+                        if has_mistakes is False:
+                            last_correct_grid = copy.deepcopy(player_grid)
+                            has_mistakes = True
+                        mistake_count += 1
+                    player_grid[i][j] = int(pygame.key.name(event.key))
+                else:
+                    mistake_count += 1
+
+
+    window.fill(white)
+
+    if solving:
+        if solve_sudoku_using_backtracking_with_animation(player_grid, window, black, white, width, height):
+            solving = False
+            print('Congratulations! You solved the Sudoku.')
+            print(f"Mistakes made: {mistake_count}")
+            time.sleep(3)
+            pygame.quit()
+            sys.exit()
+
+    draw_grid(window, width, height, black)
+    draw_numbers(window, player_grid, width, black)
+    draw_selected_cell(window, selected_cell, width, black)
+
+    pygame.display.flip()
+
+    if player_grid == solved_successfully:
+        print('Congratulations! You solved the Sudoku.')
+        print(f"Mistakes made: {mistake_count}")
+        pygame.quit()
+        sys.exit()
